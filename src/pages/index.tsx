96768296c391c5f0e9f97e4a9d945e7acf30675e
athefe <tanjavoorathefe@gmail.com>
2023-12-04 17:38:21 +0530

    small changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function index() {
-  return <div className="bg-primary text-primary-foreground p-4">middle</div>;
+  return (
+    <div className="bg-primary text-primary-foreground p-4">
+      middle container
+    </div>
+  );
 }
